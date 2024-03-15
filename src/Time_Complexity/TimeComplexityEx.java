d678618fe92eb408985c75f58b3a99702a45e9f0
sss989 <sss_9898@naver.com>
2024-03-16 01:13:37 +0900

    0316 commit

---
+++ src/Time_Complexity/TimeComplexityEx.java
@@ -0,0 +1,18 @@
+package Time_Complexity;
+import java.util.Scanner;
+public class TimeComplexityEx {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int W = sc.nextInt();
+        int H = sc.nextInt();
+        int P = sc.nextInt();
+        int Q = sc.nextInt();
+        int T = sc.nextInt();
+
+        int p = (P + T) % (2 * W);
+        int q = (Q + T) % (2 * H);
+        if (p > W) p = 2 * W - p;
+        if (q > H) q = 2 * H - q;
+        System.out.println(p + " " + q);
+    }
+}
