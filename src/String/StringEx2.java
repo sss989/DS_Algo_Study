bd8c386d23f907dc3f4f528204c85b32eaffbe44
sss989 <sss_9898@naver.com>
2024-03-18 00:13:07 +0900

    0316 commit

---
+++ src/String/StringEx2.java
@@ -0,0 +1,26 @@
+package String;
+import java.util.Scanner;
+
+public class StringEx2 {
+    public static int[] getAlphabetCountArray(String str) {
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i) - 'a']++;
+        return count;
+    }
+
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        int[] countA = getAlphabetCountArray(a);
+        int[] countB = getAlphabetCountArray(b);
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++)
+            ans += Math.abs(countA[i] - countB[i]);
+        System.out.println(ans);
+    }
+}
