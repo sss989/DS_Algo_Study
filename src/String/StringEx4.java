8fae6f24aaeab76abe6cc50316f679dd3c788471
sss989 <sss_9898@naver.com>
2024-03-18 00:17:15 +0900

    0318 commit

---
+++ src/String/StringEx4.java
@@ -0,0 +1,20 @@
+package String;
+import java.util.Scanner;
+public class StringEx4 {
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String doc = sc.nextLine();
+        String word = sc.nextLine();
+        int startIndex = 0;
+        int count = 0;
+        while (true) {
+            int findIndex = doc.indexOf(word, startIndex);
+            if (findIndex < 0)
+                break;
+            startIndex = findIndex + word.length();
+            count++;
+        }
+        System.out.println(count);
+    }
+}
